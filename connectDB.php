03ddf2eaa992e91c64480534cacf8db2eaecf39f
oven-ttta <oven-ttta@users.noreply.github.com>
2026-01-14 19:40:37 +0700

    update DB

--- connectDB.php
+++ connectDB.php
@@ -2,7 +2,7 @@
 $hostName = "45.130.164.190";
 $userName = "root";
 $passWord = "rootpassword";
-$dbName = "clothDB";
+$dbName = "clothdb";
 $conn = mysqli_connect($hostName, $userName, $passWord, $dbName);
 if (mysqli_connect_error()) {
 	echo "Connect falied : " . mysqli_connect_error();
